34b6ac45a40d6dacc9b2e13e72de2a6b666283c9
BtwAbhi <140977883+abhipampacodexdop@users.noreply.github.com>
2023-11-16 13:58:32 +0530

    Update config.py
    Gm

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/38dda0a8d147bf126d2f9.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
